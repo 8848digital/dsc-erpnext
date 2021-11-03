8b9bf4a144393c0c3cfe2d27aafbf4bb583a892b
niralisatapara <niralisatapara@gmail.com>
2021-11-03 06:05:51 +0000

    Role related changes

--- dsc_erpnext/dsc_api.py
+++ dsc_erpnext/dsc_api.py
@@ -1,31 +1,36 @@
+import frappe
 import requests
 import base64
-from os import path
+import os
 from docusign_esign import EnvelopesApi, EnvelopeDefinition, Document, Signer, CarbonCopy, SignHere, Tabs, Recipients, ApiClient, RecipientViewRequest
 
-def get_access_code(request):
+@frappe.whitelist()
+def get_access_code():
 	base_url = "https://account-d.docusign.com/oauth/auth"
 	client_id = "cd94c959-5b9a-4ac7-bffa-128be6c63639"
-	auth_url = "{0}?response_type=code&scope=signature&client_id={1}&redirect_uri={2}".format(base_url,client_id,request.build_absolute_url(reverse('auth_login')))
+	auth_url = "{0}?response_type=code&scope=signature&client_id={1}&redirect_uri={2}".format(base_url,client_id,'http://192.168.0.100')
 	return auth_url
 	#code_url = "https://account-d.docusign.com/oauth/auth?response_type=code&scope=signature&client_id=cd94c959-5b9a-4ac7-bffa-128be6c63639&&redirect_uri=http://192.168.0.100/"
 
-def auth_login(request):
+@frappe.whitelist()
+def auth_login():
 	base_url = "https://account-d.docusign.com/oauth/token"
 	client_id = "cd94c959-5b9a-4ac7-bffa-128be6c63639"
 	client_secret_key = "a346d510-cac8-4b54-a14a-5a76c29ed5d0"
 	auth_code_string = '{0}:{1}'.format(client_id,client_secret_key)
 	auth_token = base64.b64encode(auth_code_string.encode())
-
+	code = "eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQoAAAABAAYABwCAARjewZ3ZSAgAgI2eJcKd2UgCABrXc4L90CBMsRZZsAFELJ0VAAEAAAAYAAEAAAAFAAAADQAkAAAAY2Q5NGM5NTktNWI5YS00YWM3LWJmZmEtMTI4YmU2YzYzNjM5IgAkAAAAY2Q5NGM5NTktNWI5YS00YWM3LWJmZmEtMTI4YmU2YzYzNjM5NwAvGF6XS9-0T5kd_m7BuvrYMAAAAkQPwZ3ZSA.FgA-WdvWcOYaX7ByS6ht_7sjdvjWRkx-ELi-0lU-nl-0lddwIxNwMHvlF8JVie50ArKnN3R65708xiRNxdrbjVp7SzaGmayc2EGGUUcErZPICGzQpHYaXR_TpG3xf9MAYuN9KbHnYWV7jH1ELNn2xJaPeTpQsVAuEsQnv9uluLOpMb05_Vm1oLZl9x3VAj25hyaceXkDbYDBP1Xlmfqmj_jk-DnGMOh9o58rt_x8PZRK8ANj7tB8u1DwXeEXGg23NwrfZ3FHUYDckJWshIqBuO5J_1wOJFL7NRVkSnDKB0ClNNGCglm9EWjv3oYYqiwIHjgqyyTpr_OxuS-AKXgYrw"
 	req_headers = {"Authorization":"Basic {0}".format(auth_token.decode('utf-8'))}
-	post_data = {'grant_type':'authorization_code','code': request.GET.get('code')}
+	post_data = {'grant_type':'authorization_code','code': code}
 
 	r = requests.post(base_url, data=post_data, headers=req_headers)
 
 	response = r.json()
 
 	if not 'error' in response:
-		return ("{0}?token={1}".format(reverse('get_signing_url'),response['access_token']))
+		#return ("{0}?token={1}".format(reverse('get_signing_url'),response['access_token']))
+		return ("token={0}".format(response['access_token']))
+
 
 def get_signing_url():
 	"""
@@ -41,10 +46,10 @@ def get_signing_url():
 		"client_id"        : "cd94c959-5b9a-4ac7-bffa-128be6c63639",
 		"account_id"       : "cb786829-5a13-4001-8128-a06c53ead4c6",
 		"base_path"        : "https://demo.docusign.net/restapi",
-		"access_token"     : "eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQoAAAABAAUABwCA7cEkb5vZSAgAgC3lMrKb2UgCABrXc4L90CBMsRZZsAFELJ0VAAEAAAAYAAEAAAAFAAAADQAkAAAAY2Q5NGM5NTktNWI5YS00YWM3LWJmZmEtMTI4YmU2YzYzNjM5IgAkAAAAY2Q5NGM5NTktNWI5YS00YWM3LWJmZmEtMTI4YmU2YzYzNjM5MACAgPxXX5vZSDcALxhel0vftE-ZHf5uwbr62A.JFbonC4mZ5yMG3Fl0QwvGJt-XwXkox0E6tBun_sw-wo4AuVPGpcrGEV8xIc9FPB0GL018PPfk0uPbFBgvzp1NLkobAFCFaGxQ0StzOnlM8ywkQTBOLraXxWs29UeFgvOwJPQVgCWc1v-vIFqW_3GEYvhoI85PGHr_BxqwRZ3-HJFkdmCW9Re4g3oF36dRU8H8gsyAMuED4ExyT6eCw49r9gKMmlNpkTxg4oCiNmo6Xp9sR5wpOmVIRg7bg-sskkLu9DJdovM6_TquKvjcITBLnT3JdOAGWlgC2FYFwAD1UVVarG3hR5jLYJWTps7V_CZgXznEwsvoB768hP4WzumNA"
+		"access_token"     : "eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQoAAAABAAUABwAAkjgHwp3ZSAgAANJbFQWe2UgCABrXc4L90CBMsRZZsAFELJ0VAAEAAAAYAAEAAAAFAAAADQAkAAAAY2Q5NGM5NTktNWI5YS00YWM3LWJmZmEtMTI4YmU2YzYzNjM5IgAkAAAAY2Q5NGM5NTktNWI5YS00YWM3LWJmZmEtMTI4YmU2YzYzNjM5MAAAAkQPwZ3ZSDcALxhel0vftE-ZHf5uwbr62A.FVl6EvogvEO1R8PDmiFXsGq4GrMo6d1iL4H4eer7AXW49TgDb6DRYfy0Ra2SW3LkWtEWdc3JrG8CA8Fnkn3iCnVc9wrzJfDFReWu0gpeMNsxer6VrJ_hyc-jIDG1b5i4XMjBYoCe0cYrKmVj77qtpgXWP12zXKqcv59xGJzTTbN3OL81AhiWRePvrKa9E4h9Ig0AXqqkdIdgXRODxhAnncXBbIwn3fBDdPZFWfpW10rk5Dfr6kD0fN_EZ33GpH2Jk1TTgT4tfV7o_U7DF8j1jnVNnbhlEcwMzP-Er-gHiwFSpcNbxrRzShcIDmUsWOJ306j9xQjFVvY2jp50GbefEw"
 	}
 	
-	with open(path.join('/home/frappe/frappe-bench/sites/site1.local/public/files', "VACCINE.pdf"),'rb') as file:
+	with open(os.path.join('/home/frappe/frappe-bench/sites/site1.local/public/files', "VACCINE.pdf"),'rb') as file:
 		content_bytes = file.read()
 	base64_file_content = base64.b64encode(content_bytes).decode('ascii')
 
@@ -92,50 +97,17 @@ def get_signing_url():
 
 	envelope_id = results.envelope_id
 	recipient_view_request = RecipientViewRequest(
-		authentication_method=None,client_user_id=args['client_id'],
+		authentication_method="email",client_user_id=args['client_id'],
 		recipient_id = 1, return_url = "http://192.168.0.100/done",
 		user_name = args['signer_name'], email = args['signer_email']
 	)
 	results = envelope_api.create_recipient_view(args['account_id'],envelope_id,recipient_view_request=recipient_view_request)
 	
+	temp_file = envelope_api.get_document(
+		account_id=args["account_id"],
+		document_id=1,
+		envelope_id=envelope_id
+	)
+	os.rename(temp_file, "/home/frappe/frappe-bench/sites/site1.local/private/files/" + envelope_id + ".pdf")
+	frappe.db.set_value("Digital Signature","DSC-0007","signed_document","/private/files/"+ envelope_id + ".pdf")
 	return {"envelope_id": envelope_id, "redirect_url": results.url}
-
-
-
-	# envelope_args = args["envelope_args"]
-	# # Create the envelope request object
-	# envelope_definition = cls.make_envelope(envelope_args)
-
-	# # Call Envelopes::create API method
-	# # Exceptions will be caught by the calling function
-	# api_client = create_api_client(base_path=args["base_path"], access_token=args["access_token"])
-
-	# envelope_api = EnvelopesApi(api_client)
-	# results = envelope_api.create_envelope(account_id=args["account_id"], envelope_definition=envelope_definition)
-
-	# envelope_id = results.envelope_id
-
-
-
-	# # Create the Recipient View request object
-	# recipient_view_request = RecipientViewRequest(
-	# 	authentication_method = authentication_method,
-	# 	client_user_id = envelope_args['signer_client_id'],
-	# 	recipient_id = '1',
-	# 	return_url = envelope_args['ds_return_url'],
-	# 	user_name = envelope_args['signer_name'], email = envelope_args['signer_email']
-	# )
-
-
-	# # Obtain the recipient_view_url for the signing ceremony
-	# # Exceptions will be caught by the calling function
-	# results = envelope_api.create_recipient_view(args['account_id'], envelope_id,
-	# 	recipient_view_request = recipient_view_request)
-
-	# return {'envelope_id': envelope_id, 'redirect_url': results.url}
-
-
-
-	# {
-	# "url": "https://demo.docusign.net/Signing/MTRedeem/v1/490a2cc3-xxxx-xxxx-xxxx-e87be7bf44d8?slt=eyJ0eX...GB7oWg"
-	# }
--- dsc_erpnext/patches/create_workflow.py
+++ dsc_erpnext/patches/create_workflow.py
@@ -1,45 +1,148 @@
 import frappe
 
 def execute():
-	doctype = "Sales Invoice"
-	role = "Sales Manager"
-	workflow = frappe.new_doc("Workflow")
-	workflow.workflow_name = f"DSC : {doctype}"
-	workflow.document_type = "Digital Signature"
-	workflow.sent_email_alert = 0
-
-	workflow_state_list = ["Submitted","Signatory1","Signatory2"]
-	for state in workflow_state_list:
+	doc = frappe.get_single("Digital Signature Settings")
+	workflow_state_list = ["DSC 1 Completed","DSC 2 Completed","DSC 3 Completed","DSC 4 Completed"]
+	workflow_action_list = ["DSC 1","DSC 2","DSC 3","DSC 4"]
+	default_workflow_state = ["Submitted","Cancelled"]
+	
+	for state in default_workflow_state:
 		if not frappe.db.exists("Workflow State",state):
 			state_doc = frappe.new_doc("Workflow State")
 			state_doc.workflow_state_name = state
 			state_doc.save()
+	if not frappe.db.exists("Workflow Action Master","Cancel"):
+		action_doc = frappe.new_doc("Workflow Action Master")
+		action_doc.workflow_action_name = "Cancel"
+		action_doc.save()
+
+	for row in doc.documents:
+		if row.entity_type and not frappe.db.exists("Workflow",f"DSC {row.entity_type}"):
+			workflow = frappe.new_doc("Workflow")
+			workflow.workflow_name = f"DSC {row.entity_type}"
+			workflow.document_type = "Digital Signature"
+			workflow.sent_email_alert = 0
 		
-	workflow_action_list = ["DSC 1","DSC 2"]
-	for action in workflow_action_list:
-		if not frappe.db.exists("Workflow Action Master",action):
-			action_doc = frappe.new_doc("Workflow Action Master")
-			action_doc.workflow_action_name = action
-			action_doc.save()
+			if row.role_1:
+				if not frappe.db.exists("Workflow State",workflow_state_list[0]):
+					state_doc = frappe.new_doc("Workflow State")
+					state_doc.workflow_state_name = workflow_state_list[0]
+					state_doc.save()
+				if not frappe.db.exists("Workflow Action Master",workflow_action_list[0]):
+					action_doc = frappe.new_doc("Workflow Action Master")
+					action_doc.workflow_action_name = workflow_action_list[0]
+					action_doc.save()
+				workflow.append("states",{
+					"state": "Submitted",
+					"doc_status": 1,
+					"allow_edit": row.role_1
+				})
+				workflow.append("states",{
+					"state": workflow_state_list[0],
+					"doc_status": 1,
+					"allow_edit": row.role_1
+				})
+				workflow.append("states",{
+					"state": "Cancelled",
+					"doc_status": 2,
+					"allow_edit": row.role_1
+				})
+				workflow.append("transitions",{
+					"state": "Submitted",
+					"action": workflow_action_list[0],
+					"next_state": workflow_state_list[0],
+					"allowed": row.role_1
+				})
+				workflow.append("transitions",{
+					"state": "Submitted",
+					"action": "Cancel",
+					"next_state": "Cancelled",
+					"allowed": row.role_2
+				})
 
-	for state in workflow_state_list:
-		workflow.append("states",{
-			"state": state,
-			"doc_status": 1,
-			"allow_edit": role
-		})
-	workflow.append("transitions",{
-		"state": "Submitted",
-		"action": "DSC 1",
-		"next_state": "Signatory1",
-		"allowed": role
-	})
-	workflow.append("transitions",{
-		"state": "Signatory1",
-		"action": "DSC 2",
-		"next_state": "Signatory2",
-		"allowed": role
-	})
-	workflow.save(ignore_permissions=True)
+			if row.role_2:
+				if not frappe.db.exists("Workflow State",workflow_state_list[1]):
+					state_doc = frappe.new_doc("Workflow State")
+					state_doc.workflow_state_name = workflow_state_list[1]
+					state_doc.save()
+				if not frappe.db.exists("Workflow Action Master",workflow_action_list[1]):
+					action_doc = frappe.new_doc("Workflow Action Master")
+					action_doc.workflow_action_name = workflow_action_list[1]
+					action_doc.save()
+				workflow.append("states",{
+					"state": workflow_state_list[1],
+					"doc_status": 1,
+					"allow_edit": row.role_2
+				})
+				workflow.append("transitions",{
+					"state": workflow_state_list[0],
+					"action": workflow_action_list[1],
+					"next_state": workflow_state_list[1],
+					"allowed": row.role_2
+				})
+				workflow.append("transitions",{
+					"state": workflow_state_list[0],
+					"action": "Cancel",
+					"next_state": "Cancelled",
+					"allowed": row.role_2
+				})
 
+			if row.role_3:
+				if not frappe.db.exists("Workflow State",workflow_state_list[2]):
+					state_doc = frappe.new_doc("Workflow State")
+					state_doc.workflow_state_name = workflow_state_list[2]
+					state_doc.save()
+				if not frappe.db.exists("Workflow Action Master",workflow_action_list[2]):
+					action_doc = frappe.new_doc("Workflow Action Master")
+					action_doc.workflow_action_name = workflow_action_list[2]
+					action_doc.save()
+				workflow.append("states",{
+					"state": workflow_state_list[2],
+					"doc_status": 1,
+					"allow_edit": row.role_3
+				})
+				workflow.append("transitions",{
+					"state": workflow_state_list[1],
+					"action": workflow_action_list[2],
+					"next_state": workflow_state_list[2],
+					"allowed": row.role_3
+				})
+				workflow.append("transitions",{
+					"state": workflow_state_list[1],
+					"action": "Cancel",
+					"next_state": "Cancelled",
+					"allowed": row.role_3
+				})
 
+			if row.role_4:
+				if not frappe.db.exists("Workflow State",workflow_state_list[3]):
+					state_doc = frappe.new_doc("Workflow State")
+					state_doc.workflow_state_name = workflow_state_list[3]
+					state_doc.save()
+				if not frappe.db.exists("Workflow Action Master",workflow_action_list[3]):
+					action_doc = frappe.new_doc("Workflow Action Master")
+					action_doc.workflow_action_name = workflow_action_list[3]
+					action_doc.save()
+				workflow.append("states",{
+					"state": workflow_state_list[3],
+					"doc_status": 1,
+					"allow_edit": row.role_4
+				})
+				workflow.append("transitions",{
+					"state": workflow_state_list[2],
+					"action": workflow_action_list[3],
+					"next_state": workflow_state_list[3],
+					"allowed": row.role_4
+				})
+				workflow.append("transitions",{
+					"state": workflow_state_list[2],
+					"action": "Cancel",
+					"next_state": "Cancelled",
+					"allowed": row.role_4
+				})
+			
+			workflow.save(ignore_permissions=True)
+			print(workflow.name)
+			row.db_set('workflow',workflow.name)
+			
+	frappe.db.commit()
